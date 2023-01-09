7d39d630b82fbf5485741685051f0e49a3527677
nehanims <neha.h.nims@gmail.com>
2023-01-09 15:32:38 -0700

    Adds the None option back

--- starter/src/BookShelfChanger.js
+++ starter/src/BookShelfChanger.js
@@ -4,12 +4,13 @@ const BookShelfChanger = ({ shelf, onShelfChange }) => {
     return (
         <div className="book-shelf-changer">
             <select value={shelf} onChange={onShelfChange}>
-                <option value="none" disabled>
+                <option value="nothing" disabled>
                     Move to...
                 </option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
+                <option value="none">None</option>
             </select>
 
         </div>
